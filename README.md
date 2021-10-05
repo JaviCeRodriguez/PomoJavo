5beff88e8c773c19c7b3f945648e7a00aca098a2
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-10-04 21:29:39 -0300

    Add timers changes w/ onClick's buttons

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Clon de Pomofocus.io
 
-## Stack
+## Tecnologías Principales
 
 - Parcel v2
 - React
-- ...
+- Styled Components
 
 ## Funciones
 
-- [ ] Timers para modos **Concentración**, **Descanso corto** y **Descanso largo**.
-- [ ] Botón de comienzo y pausa
+- [x] Timers para modos **Pomodoro**, **Short** y **Long**.
+- [x] Botón de comienzo y pausa
 - [ ] Configuración de tiempos
 - [ ] Estadísticas
 - [ ] Tareas
 
 ## Paletas de colores:
 
-Pomodoro: https://coolors.co/1f271b-4c376e-794bc4-cbb9a8-dcc7be
-Short break: https://coolors.co/1f271b-0b4f6c-145c9e-cbb9a8-dcc7be
-Long break: https://coolors.co/1f271b-33d02d-7fd455-cbb9a8-dcc7be
+**Pomodoro**: https://coolors.co/1f271b-4c376e-794bc4-cbb9a8-dcc7be
+**Short break**: https://coolors.co/1f271b-0b4f6c-145c9e-cbb9a8-dcc7be
+**Long break**: https://coolors.co/1f271b-33d02d-7fd455-cbb9a8-dcc7be
--- src/components/Pomodoro/pomodoro.js
+++ src/components/Pomodoro/pomodoro.js
@@ -3,9 +3,21 @@ import Button from "../Button/button";
 import { StyledPomodoro } from "./pomodoro.styles";
 
 const Pomodoro = () => {
-  let pomodoroTimer = 25 * 60
-  const [counter, setCounter] = useState(pomodoroTimer)
+  const initialMode = {
+    'Pomodoro': true,
+    'Short': false,
+    'Long': false
+  }
+  const initialModeTimer = {
+    'Pomodoro': 25,
+    'Short': 10,
+    'Long': 15
+  }
+  let initialTimer = initialModeTimer['Pomodoro'] * 60
+
+  const [counter, setCounter] = useState(initialTimer)
   const [toggleCount, setToggleCount] = useState(false)
+  const [mode, setMode] = useState(initialMode)
 
   const twoDigits = (num) => {
     return num.toLocaleString('en-US', {
@@ -22,12 +34,25 @@ const Pomodoro = () => {
     }
   }, [counter, toggleCount]);
 
+  const selectMode = (e) => {
+    setToggleCount(false)
+    const key = e.target.value
+    let modes = mode
+
+    Object.keys(modes).map((k) => {
+      k === key ? modes[k] = true : modes[k] = false
+    })
+
+    setMode(modes)
+    setCounter(initialModeTimer[key] * 60)
+  }
+
   return (
     <StyledPomodoro>
       <div className="mode-wrapper">
-        <Button text="Pomodoro" />
-        <Button text="Short" />
-        <Button text="Long" />
+        <Button text="Pomodoro" funcEvent={selectMode} />
+        <Button text="Short" funcEvent={selectMode} />
+        <Button text="Long" funcEvent={selectMode} />
       </div>
       <h1>{twoDigits(Math.floor(counter / 60))}:{twoDigits(Math.floor(counter % 60))}</h1>
       <Button text={toggleCount ? "PAUSE" : "START"} type="full" funcEvent={startCount} />
